6fc3f4dfa700445e722da09881bb01dff689f5de
ktlz <katya.llazorenko@gmail.com>
2025-01-21 23:19:47 +0100

    add theme context

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,20 @@
+import { useContext } from 'react';
 import Link from "next/link"
 import { Button } from "./ui/button"
 import Nav from "./Nav"
 import MobileNav from "./MobileNav"
+import { ThemeContext } from "@/app/layout"
 
 const Header = () => {
+  const theme = useContext(ThemeContext);
+  const textColor = theme === 'dark' ? 'text-white' : 'text-primary'
+
   return (
-    <header className="py-8 xl:py-12 text-white">
+    <header className={`py-8 xl:py-12 ${textColor}`}>
       <div className="container mx-auto flex justify-between items-center">
         {/* logo */}
         <Link href="/">
-          <h1 className="text-4xl font-semibold">Kate <span className="text-accent">.</span></h1>
+          <h1 className={`text-4xl font-semibold ${textColor}`}>Kate <span className="text-accent">.</span></h1>
         </Link>
         {/* desktop nav */}
         <div className="hidden xl:flex items-center gap-8">
